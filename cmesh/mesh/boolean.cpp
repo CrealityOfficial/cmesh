ca1708fb41df39545b1322a84e2999a73c9329f2
anoob <anoob@sina.cn>
2022-01-04 12:20:14 +0800

    boolean

--- cmesh/mesh/boolean.cpp
+++ cmesh/mesh/boolean.cpp
@@ -1,6 +1,5 @@
 #include "boolean.h"
 
-#if defined(WIN32) && defined(USE_CGAL)
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Polygon_mesh_processing/corefinement.h>
 #include <CGAL/Surface_mesh.h>
@@ -208,12 +207,4 @@ namespace cmesh
     }
 }
 
-#else
-namespace cmesh
-{
-    trimesh::TriMesh* cxBooleanOperateMeshObj(trimesh::TriMesh* Mesh1, trimesh::TriMesh* Mesh2, cxBoolean_operation_type typeindex)
-    {
-        return nullptr;
-    }
-}
-#endif // WIN32
+
--- cmesh/mesh/boolean.h
+++ cmesh/mesh/boolean.h
@@ -3,7 +3,6 @@
 #include "cmesh/interface.h"
 #include "trimesh2/TriMesh.h"
 
-
 namespace cmesh
 {
     CMESH_API enum  class cxBoolean_operation_type
@@ -13,6 +12,7 @@ namespace cmesh
         CX_TM1_MINUS_TM2,
         CX_TM2_MINUS_TM1,
     };
+
     CMESH_API trimesh::TriMesh* cxBooleanOperateMeshObj(trimesh::TriMesh* Mesh1, trimesh::TriMesh* Mesh2, cxBoolean_operation_type typeindex);
 }
 #endif // MMESH_BOOLEAN_1607481576423_H
