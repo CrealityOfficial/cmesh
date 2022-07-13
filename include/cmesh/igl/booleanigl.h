069bebce834f9bbb086ece344f40f2c8e8b88516
wangjiang <wangjiang@creality.com>
2022-07-13 17:25:16 +0800

    fix:[cmesh]libigl库的整理
    Change-Id: Ieac386646ad562bc7ed3ec1d38edc0ac1d4b8bfc

--- include/cmesh/igl/booleanigl.h
+++ include/cmesh/igl/booleanigl.h
@@ -2,12 +2,13 @@
 #define MMESH_BOOLEANIGL_1607481576423_H
 #include "cmesh/interface.h"
 #include "trimesh2/TriMesh.h"
+#include "ccglobal/tracer.h"
 
 namespace cmesh
 {
 
-    CMESH_API trimesh::TriMesh* cxBooleanOperateMeshIGLObj(trimesh::TriMesh* Mesh1, trimesh::TriMesh* Mesh2);
+    CMESH_API trimesh::TriMesh* cxBooleanOperateMeshIGLObj(trimesh::TriMesh* Mesh1, trimesh::TriMesh* Mesh2, ccglobal::Tracer* tracer = nullptr);
 
-    CMESH_API trimesh::TriMesh* cxBooleanOperateMeshIGLObj(std::string& Mesh1Name, std::string& Mesh2Name);
+    CMESH_API trimesh::TriMesh* cxBooleanOperateMeshIGLObj(std::string& Mesh1Name, std::string& Mesh2Name, ccglobal::Tracer* tracer = nullptr);
 }
 #endif // MMESH_BOOLEANIGL_1607481576423_H
